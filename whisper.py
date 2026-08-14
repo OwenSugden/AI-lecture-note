623a37ef939ac732655bb385a77f9995ea4a8576
OwenSugden <owen.sugden04@gmail.com>
2026-08-14 21:47:50 +1200

    Debugged whisper.py due to errors not finding the .dll file correctly

--- whisper.py
+++ whisper.py
@@ -1,7 +1,17 @@
-from faster_whisper import WhisperModel
-from datetime import timedelta
+import os
+os.add_dll_directory(r"C:\Users\User\AppData\Local\Programs\Python\Python313\Lib\site-packages\nvidia\cublas\bin")
+import ctypes
+ctypes.WinDLL("cublas64_12.dll")                    
 
-audio_path = "/"
+from faster_whisper import WhisperModel
+import glob
+import subprocess
+
+week_number = 3
+
+video_path = glob.glob(f"lectures/weeks/week-{week_number}/*.mp4")[0]
+subprocess.run(["ffmpeg", "-i", video_path, f"lectures/weeks/week-{week_number}/week-{week_number}.wav"])
+audio_path = glob.glob(f"lectures/weeks/week-{week_number}/*.wav")[0]
 
 model = WhisperModel("medium", device="cuda", compute_type="float16")
 
@@ -15,12 +25,11 @@ def timeconvert(time):
 
     return f"{hours:02}:{minutes:02}:{seconds:02},{milliseconds:03}"
 
-file = open("myfile.srt", "w") 
+file = open(f"lectures/weeks/week-{week_number}/week-{week_number}.srt", "w") 
 
 for count, segment in enumerate(segments, 1):
     startTime = timeconvert(segment.start)
     endTime = timeconvert(segment.end)
-    
     file.write(f"{str(count)}\n{startTime} -->  {endTime}\n{segment.text}\n\n")
 
 file.close()
